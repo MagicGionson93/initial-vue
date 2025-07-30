aadb27f9c227d60062077cf58c2863a753f0ce7f
MagicGionson93 <gpasini93@gmail.com>
2025-07-30 17:19:26 +0200

    added some lines

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,35 +1,51 @@
 <script setup>
-
 import { ref } from "vue";
-const counter = ref(0)
+import { reactive, computed } from "vue";
+
+const counter = ref(0);
 
 function decrease() {
-    --counter.value;
+  --counter.value;
 }
 
 function increase() {
-    ++counter.value;
+  ++counter.value;
 }
 
+const state = reactive({ count: 100 });
+
+const label = ref("Prova Label");
 </script>
 <template>
+  <div>
     <section>
-        <button @click="decrease()">- decrementa</button>
-        <button @click="increase()">+ incrementa</button>
-        <p>{{ counter }}</p>
+      <button @click="decrease()">- decrementa</button>
+      <button @click="++counter">+ incrementa</button>
+      <p>Counter: {{ counter }}</p>
     </section>
+    <section>
+      <button @click="label = 'Nuovo Valore '">Altera Label</button>
+      <div>Label: {{ label }}</div>
+    </section>
+    <section>
+      <h2>Stato reattivo con reactive</h2>
+      <button @click="state.count++">Incrementa count</button>
+      <div>Count: {{ state.count }}</div>
+      <div>Stato: {{ state.count < 90 ? "Limite superato" : "Perfetto" }}</div>
+    </section>
+  </div>
 </template>
 
 <style scope>
 section {
-    padding: 20px;
-    background-color: lightblue;
-    margin: 20px 0;
+  padding: 20px;
+  background-color: lightblue;
+  margin: 20px 0;
 }
 
 button {
-    padding: 6px;
-    background-color: blueviolet;
-    border-radius: 6px;
+  padding: 6px;
+  background-color: blueviolet;
+  border-radius: 6px;
 }
 </style>
